a03e0c6a9fef87fa4ca4c260598ddd4b6a8bf34d
hackerlove123 <149338972+hackerlove123@users.noreply.github.com>
2025-02-28 11:45:44 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const TelegramBot = require('node-telegram-bot-api'),
     { spawn } = require('child_process'), // Sử dụng spawn thay vì exec
-    token = '7935173392:AAFMBexmTDJ7aNveu4CkgtCbM4Q92mRJK0A',
+    token = '7935173392:AAHxrhAJRirK5O2faumWJLZlJtfOZMxWsZM',
     adminId = 7371969470,
     allowedGroupIds = new Set([-1002411881962, -1002334544605, -1002365124072, -1002345371324, 998877665]),
     bot = new TelegramBot(token, { polling: true }),
