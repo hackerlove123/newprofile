0cb2a169591a2d7638a22c654cf6db0ec8d870e2
hackerlove123 <149338972+hackerlove123@users.noreply.github.com>
2025-02-28 11:14:23 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -38,7 +38,7 @@ const initBot = () => {
             return; // Bỏ qua lệnh mà không thông báo
         }
 
-        if (!isAdmin && !isGroup) return bot.sendMessage(chatId, '❌ Bạn không có quyền sử dụng liên hệ: @Sasuke_1122.', { parse_mode: 'HTML' });
+        if (!isAdmin && !isGroup) return bot.sendMessage(chatId, '❌ Bạn không có quyền sử dụng liên hệ: https://t.me/NeganSSHConsole.', { parse_mode: 'HTML' });
         if (!text || !['http://', 'https://', 'exe ', '/help'].some(cmd => text.startsWith(cmd))) return;
         if (text === '/help') return bot.sendMessage(chatId, helpMessage, { parse_mode: 'HTML' });
 
